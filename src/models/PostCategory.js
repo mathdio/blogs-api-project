9237e90cb6ad9b8ed3af4ba89835834a52661571
Matheus Diógenes <mathdiogenes@gmail.com>
2023-01-26 18:35:50 -0300

    feat: R11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
+    categoryId: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
+  },
+  {
+    timestamps: false,
+    tableName: 'posts_categories',
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
