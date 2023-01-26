d1dabcbe71b97d9a1a55bbb64404c72c3e2b0e75
Matheus Diógenes <mathdiogenes@gmail.com>
2023-01-26 16:05:29 -0300

    feat: R03

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const loginValidation = require('./middlewares/loginValidation');
+const usersController = require('./controllers/users.controller');
 
 // ...
 
@@ -7,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 // ...
+app.post('/login', loginValidation, usersController.validateLogin);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
